42b06b4031515450f6d8d01d256c06613ab96eed
pooyajjj <king@kali.King>
2023-01-05 15:33:41 +0330

    model user creatad with abstractbaseuser and registered

--- PAA/accounts/models.py
+++ PAA/accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=225, unique=True, null=True, blank=True)
+    email = models.EmailField(max_length=225, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=225, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email',]
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
